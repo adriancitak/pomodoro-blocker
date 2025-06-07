990aab4e28b7daa2a7b1861218f1e64649c818c9
adriancitak <75241775+adriancitak@users.noreply.github.com>
2025-06-07 10:12:26 +0300

    Added pause/play components

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,7 +1,30 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton'
+import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
+
 export default function Timer(){
+
+    const red = '#f54e4e'
+    const green = '#4aec8c'
 
 
     return (
-        <h1>Timer Component</h1>
+        <div>
+            <CircularProgressbar value={90} text={'90%'} styles={buildStyles({
+                textColor: '#fff',
+                pathColor: red,
+                tailColor: 'rgba(255,255,255,0.2)',
+            })}/>
+            <div style={{marginTop:'20px'}}>
+                <PlayButton />
+                <PauseButton />
+            </div>
+            <div style={{marginTop:'20px'}}>
+                <SettingsButton />
+            </div>
+
+        </div>
     )
 }
