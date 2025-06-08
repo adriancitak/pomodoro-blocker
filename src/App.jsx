c389036fdfec8880dc32c508c83c82b99a3bbf62
adriancitak <75241775+adriancitak@users.noreply.github.com>
2025-06-08 14:23:22 +0300

    Refactor- starting from scratch

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Timer from './Timer'
-import './App.css'
-import Settings from './Settings'
-import SettingsContext from './SettingsContext'
 
-function App() {
-  const [showSettings, setShowSettings] = useState(false)
-  const [workMinutes, setWorkMinutes] = useState(50)
-  const [breakMinutes, setBreakMinutes] = useState(10)
+export default function App(){
+
+
 
   return (
+    <>
+      <Timer />
+    </>
 
-    <main>
-        <SettingsContext.Provider value={{
-          showSettings,
-          setShowSettings,
-          workMinutes,
-          breakMinutes,
-          setWorkMinutes,
-          setBreakMinutes
-        }}>
-            {showSettings ? <Settings /> : <Timer />}
-        </SettingsContext.Provider>
-        
-        
-    </main>
   )
-   
-      
-  
 }
-
-export default App
